c3af2e59b8904a68c3338877fe637377dab5a795
shivanirmishra <shivani007mishra@gmail.com>
2020-07-01 17:49:36 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
+        "http://shiv.prashplus.com",
         
         {
         // how many entries do you want?
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "shivanirmishra");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "shivanirmishra", selector: "#ghfeed" });
 
 
 });
